0ddb8d037c0efcc69c8e0853070dbe176da2c3ce
Tanaka Kodai <kodai030513@gmail.com>
2023-07-08 20:56:01 +0900

    ログイン時のみセッション発行に変更

--- communityDetail.php
+++ communityDetail.php
@@ -4,12 +4,31 @@ require_once __DIR__ . '/SQL.php';
 require_once __DIR__ . '/Func.php';
 require_once __DIR__ . '/url.php';
 
-if(!empty($_GET['data']))
+
+$community_id = "";
+if(!empty($_SESSION['login_user']['community_id']) && !empty($_GET['data']))
 {
-    $_SESSION['community_id'] = $_GET['data'];
+    unset($_SESSION['login_user']['community_id']);
 }
 
-$whereCondition = "community_id = '{$_SESSION['community_id']}'";
+if(!empty($_SESSION['login_user']['user_id']))
+{
+    if(!empty($_GET['data']))
+    {
+        $_SESSION['login_user']['community_id'] = $_GET['data'];
+        $community_id = $_SESSION['login_user']['community_id'];
+    }
+    if(isset($_POST['message-create']))
+    {
+        $community_id = $_SESSION['login_user']['community_id'];
+    }
+    
+} else
+{
+    $community_id = $_GET['data'];
+}
+
+$whereCondition = "community_id = '{$community_id}'";
 $getcountCommunity = SQL::db_getcount('inventorymanage', 'community', $whereCondition);
 $existsCommunity = false;
 if($getcountCommunity > 0)
@@ -21,11 +40,11 @@ if(isset($_POST['message-create']) && $existsCommunity && $_SESSION['login_user'
 {
     try
     {
-        $message['community_id'] = $_SESSION['community_id'];
+        $message['community_id'] = $community_id;
         $message['user_id'] = $_SESSION['login_user']['user_id'];
         $message['sentence'] = $_POST['sentence'];
         SQL::db_insert('inventorymanage', 'message', $message);
-        header("Location: {$communityDetailPageUrl}?data={$_SESSION['community_id']}");
+        header("Location: {$communityDetailPageUrl}?data={$community_id}");
         exit;
     } catch(PDOException $e)
     {
@@ -39,7 +58,7 @@ if($existsCommunity)
     try
     {
         $messageColumns = ['nickname', 'sentence', 'create_date'];
-        $messageCondition = "community_id = {$_SESSION['community_id']} order by create_date desc";
+        $messageCondition = "community_id = {$community_id} order by create_date desc";
         $messageJoin = "msg inner join userdata ud on msg.user_id = ud.user_id";
         $messagedata = SQL::db_aliasFetchAll('inventorymanage', 'message', $messageColumns, $messageCondition, $messageJoin);
     } catch (PDOException $e)
